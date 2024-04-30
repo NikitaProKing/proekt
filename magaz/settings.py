65e194d2dec03628e3ce205c11f1ef500ba587d7
NikitaProKing <164020071+NikitaProKing@users.noreply.github.com>
2024-04-30 19:07:43 +0600

    Update settings.py

--- magaz/settings.py
+++ magaz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fum#+l10w^@tl7cgpu58%7b+-w%nkzz_-o*7$%llv*vvd!kfzz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["91.210.169.152"]
 
 
 # Application definition
